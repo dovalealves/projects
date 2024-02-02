3299614a49530081c5de6cf605d325972c913efa
dovalealves <pedrodovalealves@gmail.com>
2024-02-02 15:22:23 +0000

    add and find added to rest controller

--- SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ SpringBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,8 +2,7 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,7 +11,7 @@ public class EmployeeRestController {
 
     private EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeService theEmployeeService){
+    public EmployeeRestController(EmployeeService theEmployeeService) {
         employeeService = theEmployeeService;
     }
 
@@ -21,4 +20,32 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    // add mapping for GET /employees/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee Id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    // add mapping for POST /employees/{employeeId}
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // just in case they pass an id in JSON, set id to 0 -- this is to force a save of the new item instead of update
+        employee.setId(0);
+
+        Employee dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
+    }
+
 }
