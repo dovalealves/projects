0033426ac00cacd829adee246c2bb240cdbd9683
dovalealves <pedrodovalealves@gmail.com>
2024-02-06 14:47:32 +0000

    spring data config, pagination and sorting

--- SpringBoot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ SpringBoot/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // Extends Jpa Repo - employee as entity and integer (Id) as primary key
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // no implementation required
