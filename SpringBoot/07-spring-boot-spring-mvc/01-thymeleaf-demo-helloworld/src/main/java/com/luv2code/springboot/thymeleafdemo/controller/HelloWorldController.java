c11c2baade9a9cf951ce017f0231eac0acb7dced
dovalealves <pedrodovalealves@gmail.com>
2024-03-06 12:15:18 +0000

    adding data to model

--- SpringBoot/07-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ SpringBoot/07-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,26 @@ public class HelloWorldController {
     // controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+
+    // controller method to read form data and add data to the model
+    @RequestMapping("/processForm-v2")
+    public String letsShout(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        //convert data to upper Case
+        theName = theName.toUpperCase();
+
+        // create message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
