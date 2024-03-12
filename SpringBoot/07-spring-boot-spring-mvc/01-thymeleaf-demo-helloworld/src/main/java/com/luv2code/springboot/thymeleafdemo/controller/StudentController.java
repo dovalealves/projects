984f42aff6ba3c3a820c6cbd38c9cd94f7aeec7b
dovalealves <pedrodovalealves@gmail.com>
2024-03-12 12:55:02 +0000

    form and radio buttons data binding with spring MVC

--- SpringBoot/07-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ SpringBoot/07-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+    @Value("${languages}")
+    private List<String> languages;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -19,6 +27,12 @@ public class StudentController {
 
         // add student to the model
         theModel.addAttribute("student", student);
+
+        // add list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        // add list of languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
