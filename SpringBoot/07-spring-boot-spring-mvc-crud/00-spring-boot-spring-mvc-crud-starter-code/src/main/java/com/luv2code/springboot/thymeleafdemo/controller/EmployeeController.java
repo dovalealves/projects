1506ee05389575ec8669cf0b9879cfcd18f6e423
dovalealves <pedrodovalealves@gmail.com>
2024-03-14 12:41:30 +0000

    Thymeleaf and spring boot CRUD project

---
+++ SpringBoot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        // get the employee from the DB
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        model.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}
